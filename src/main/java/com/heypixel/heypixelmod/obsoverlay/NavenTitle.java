5197b5e8a1ce9b082aa1c392978ed67519f8af52
xiaodu <2043377141@qq.com>
2025-08-21 12:43:59 +0800

    fix Title

--- src/main/java/com/heypixel/heypixelmod/obsoverlay/NavenTitle.java
+++ src/main/java/com/heypixel/heypixelmod/obsoverlay/NavenTitle.java
@@ -11,7 +11,7 @@ import static com.heypixel.heypixelmod.obsoverlay.modules.Module.mc;
 public class NavenTitle {
     public String getTitle() {
         final SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
-        String Title = CLIENT_NAME + " | " + Version.getVersion() + " | "+ NavenUser.getUsername() +" | " + StringUtils.split(mc.fpsString, " ")[0] + " FPS | " + format.format(new Date());
+        String Title = CLIENT_NAME + " | " + Version.getVersion() + " | " + StringUtils.split(mc.fpsString, " ")[0] + " FPS | " + format.format(new Date());
         return Title;
     }
 }
