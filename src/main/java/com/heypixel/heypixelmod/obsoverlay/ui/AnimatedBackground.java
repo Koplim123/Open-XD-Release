32b4ba60cd8fba30757f075de00df5a5852fe862
xiaodu <2043377141@qq.com>
2025-09-19 22:36:28 +0800

    new welcome

--- src/main/java/com/heypixel/heypixelmod/obsoverlay/ui/AnimatedBackground.java
+++ src/main/java/com/heypixel/heypixelmod/obsoverlay/ui/AnimatedBackground.java
@@ -3,7 +3,7 @@ package com.heypixel.heypixelmod.obsoverlay.ui;
 import net.minecraft.resources.ResourceLocation;
 
 public class AnimatedBackground {
-    private static final String FRAME_LOCATION_PATTERN = "heypixel:textures/ui/background/frame_%04d.png";
+    private static final String FRAME_LOCATION_PATTERN = "heypixel:textures/ui/background/frame_%04d.jpg";
     private final int frameCount;
     private int currentFrame = 1;
     private int tickCounter = 0;
@@ -16,7 +16,7 @@ public class AnimatedBackground {
     public AnimatedBackground(int frameCount, int fps) {
         this.frameCount = frameCount;
         // Minecraft 每秒 20 tick, 计算每隔多少 tick 更新一帧
-        this.ticksPerFrame = Math.max(1, 20 / fps);
+        this.ticksPerFrame = Math.max(1, 10 / fps);
     }
 
     public void tick() {
